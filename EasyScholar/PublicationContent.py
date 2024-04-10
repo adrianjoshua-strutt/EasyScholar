e2a77ced4806212c6588b41a006d2c1010ebfbd6
Adrian Joshua Strutt <adrianjoshua.strutt@gmail.com>
2024-04-10 20:52:45 +0200

    Added: PublicationContet, ScholarScraper. Single Result Scraping Possible

---
+++ EasyScholar/PublicationContent.py
@@ -0,0 +1,46 @@
+import bs4
+import requests
+from bs4 import BeautifulSoup
+import urllib.parse
+
+from EasyScholar.ScholarScraper import getPublicationContent
+
+
+class PublicationContent:
+
+    def __init__(self, title):
+        if type(title) is str:
+            self.content = getPublicationContent(title)
+            self.title = title
+        else:
+            raise TypeError("PublicationContent should be provided with string, " + str(title) + "provided")
+
+    def __getDivInfo(self):
+        list_div_info = self.content.find_all("div", {"class": "gs_a gs_fma_p"})
+        div_info = list_div_info[0]
+        return div_info
+
+    def getDate(self) -> str:
+        div_info_text = self.__getDivInfo().get_text()
+        date = div_info_text.split("•")[0].strip()[-4:]
+        return date
+
+    def getPublicationWebsite(self) -> str:
+        div_info_text = self.__getDivInfo().get_text()
+        publication_website = div_info_text.split("•")[1].strip()
+        return publication_website
+
+    def getPublicationUrl(self) -> str:
+        href_title = self.content.find_all(lambda tag: tag.name == "a" and self.title in tag.text)[0]
+        publication_url = href_title['href']
+        return publication_url
+
+    def toDict(self) -> dict:
+        return {
+            "date": self.getDate(),
+            "publication_website": self.getPublicationWebsite(),
+            "publication_url": self.getPublicationUrl(),
+        }
+
+    def __str__(self) -> str:
+        return self.toDict().__str__()
---
+++ main.py
@@ -0,0 +1,7 @@
+from EasyScholar.PublicationContent import PublicationContent
+
+content = PublicationContent('Discriminative unsupervised feature learning with convolutional neural networks')
+
+print(content.getDate()) # Output: 2014
+print(content.getPublicationWebsite()) # Output: proceedings.neurips.cc
+print(content.getPublicationUrl()) # Output: https://proceedings.neurips.cc/paper/2014/hash/07563a3fe3bbe7e3ba84431ad9d055af-Abstract.html
